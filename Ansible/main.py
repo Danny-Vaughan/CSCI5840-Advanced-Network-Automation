4b80f47607015ed03547e0c7dc1d4f3ff8345da5
Danny Vaughan <danny.vaughan1080@yahoo.com>
2025-09-20 19:51:06 -0600

    Added Ansible Folder and Files

---
+++ Ansible/main.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+
+import os
+import mk_new_play
+from netmiko import ConnectHandler
+import concurrent.futures as cf
+import subprocess
+
+site_yml_location = "/home/student/CSCI5840-Advanced-Network-Automation/Ansible/site.yml"
+router_tasks_yml_location = "/home/student/CSCI5840-Advanced-Network-Automation/Ansible/roles/router/tasks/main.yml"
+switch_tasks_yml_location = "/home/student/CSCI5840-Advanced-Network-Automation/Ansible/roles/switch/tasks/main.yml"
+edge_router_tasks_yml_location = "/home/student/CSCI5840-Advanced-Network-Automation/Ansible/roles/edge_router/tasks/main.yml"
+
+
+site_yml_content = """
+---
+- name: Configure Routers
+  hosts: router
+  connection: local
+  gather_facts: no
+  roles:
+    - role: router
+
+- name: Configure Edge Routers
+  hosts: edge_router
+  connection: local
+  gather_facts: no
+  roles:
+    - role: edge_router
+
+- name: Configure Switches
+  hosts: switch
+  connection: local
+  gather_facts: no
+  roles:
+    - role: switch
+"""
+
+router_tasks_yml_content = """
+---
+- name: Load Variables
+  include_vars:
+    file: "/home/student/CSCI5840-Advanced-Network-Automation/Ansible/roles/router/vars/router.yml"
+
+- name: Generate configuration files
+  template: 
+    src: router.j2 
+    dest: "/home/student/CSCI5840-Advanced-Network-Automation/Ansible/configs/{{item.hostname}}.txt"
+  loop: "{{ devices | default([]) }}"
+  when: devices is defined
+"""
+switch_tasks_yml_content = """
+---
+- name: Load Variables
+  include_vars:
+    file: "/home/student/CSCI5840-Advanced-Network-Automation/Ansible/roles/switch/vars/switch.yml"
+
+- name: Generate configuration files
+  template: 
+    src: switch.j2
+    dest: "/home/student/CSCI5840-Advanced-Network-Automation/Ansible/configs/{{item.hostname}}.txt"
+  loop: "{{ devices | default([]) }}"
+  when: devices is defined
+"""
+edge_router_tasks_yml_content = """
+---
+- name: Load Variables
+  include_vars:
+    file: "/home/student/CSCI5840-Advanced-Network-Automation/Ansible/roles/edge_router/vars/edge_router.yml"
+
+- name: Generate configuration files
+  template: 
+    src: edge_router.j2 
+    dest: "/home/student/CSCI5840-Advanced-Network-Automation/Ansible/configs/{{item.hostname}}.txt"
+  loop: "{{ devices | default([]) }}"
+  when: devices is defined
+"""
+
+
+# this function sets up the ansible files needed for template creation. It will create a site.yml and then a main.yml in the tasks folder for each role
+def mk_playbook_files():
+    with open(site_yml_location, "w") as site_file:
+        site_file.write(site_yml_content)
+    print("Created site.yml")
+
+    with open(router_tasks_yml_location, "w") as r_task_file:
+        r_task_file.write(router_tasks_yml_content)
+    print("Created router main.yml in tasks folder")
+
+    with open(switch_tasks_yml_location, "w") as s_task_file:
+        s_task_file.write(switch_tasks_yml_content)
+    print("Created switch main.yml in tasks folder")
+
+    with open(edge_router_tasks_yml_location, "w") as e_task_file:
+        e_task_file.write(edge_router_tasks_yml_content)
+    print("Created edge_router main.yml in tasks folder")
+
+    # runs the mk_new_play.py script that creates a yml file with the formatted info from requirements.csv
+    mk_new_play.yaml_file_creator("requirements.csv")
+
+
+
+# runs the ansible playbook
+def mk_play_run():
+    print("Running ansible-playbook")
+    run = subprocess.run(["ansible-playbook", "-i", "inventory.yml", "/home/student/CSCI5840-Advanced-Network-Automation/Ansible/site.yml"], stdout=subprocess.PIPE, text=True)
+    print(run.stdout)
+    if run.returncode == 0:
+        print("Playbook completed")
+
+
+
+# basic function to use netmiko to configure a device
+def Config(man_ip, config_file): 
+    try:
+
+        login = {
+                "device_type": "cisco_ios",
+                "host": f"{man_ip}",
+                "username": f"team",
+                "password": f"team",
+                "secret": f"netman",
+        }
+
+        with ConnectHandler(**login) as net_connect:
+            print(f"Logged in to {man_ip} as team")
+            net_connect.enable()
+            output = net_connect.send_config_from_file(config_file)
+            print(f"{man_ip} configured")
+    except KeyboardInterrupt:
+        print("Exiting")
+
+
+# uses the config function and concurrency to run the function for each router at the same time   
+def bgp_config():
+    config_ip_list = ["198.51.100.10", "198.51.100.20", "198.51.100.30", "198.51.100.40", "198.51.100.50", "198.51.100.60", "198.51.100.70", "198.51.100.80"]
+    config_file_list = ["/etc/ansible/configs/R1.txt", "/etc/ansible/configs/R2.txt", "/etc/ansible/configs/R3.txt", "/etc/ansible/configs/R4.txt", "/etc/ansible/configs/R5.txt", "/etc/ansible/configs/R6.txt", "/etc/ansible/configs/R7.txt", "/etc/ansible/configs/R8.txt"]
+    with cf.ThreadPoolExecutor(max_workers=4) as executor:
+        futures = []
+        for ip, config_file in zip(config_ip_list, config_file_list):
+            futures.append(executor.submit(Config, man_ip=str(ip), config_file=config_file))
+
+
+# main function 
+def main():
+    mk_playbook_files()
+    mk_play_run()
+
+
+if __name__ == "__main__":
+    main()
